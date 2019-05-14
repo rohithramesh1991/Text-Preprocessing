6818a2b696296b463ec1e16cd8e44e224fcb71f4
rohithramesh1991 <49020018+rohithramesh1991@users.noreply.github.com>
2019-05-14 23:14:21 +0530

    Add files via upload

---
+++ Text Preprocessing_codes.py
@@ -0,0 +1,27 @@
+﻿#Text pre-processing
+"""removes punctuation, stopwords, and returns a list of the remaining words, or tokens"""
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+nltk.download('stopwords')
+nltk.download('wordnet')
+
+
+import string
+def text_process(text):
+    '''
+    Takes in a string of text, then performs the following:
+    1. Remove all punctuation
+    2. Remove all stopwords
+    3. Return the cleaned text as a list of words
+    4. Remove words
+    '''
+    stemmer = WordNetLemmatizer()
+    nopunc = [char for char in text if char not in string.punctuation]
+    nopunc = ''.join([i for i in nopunc if not i.isdigit()])
+    nopunc =  [word.lower() for word in nopunc.split() if word not in stopwords.words('english')]
+    return [stemmer.lemmatize(word) for word in nopunc]
+
+#testing the function with a sample text#
+sample_text = "Hey There! This is a Sample review, which 123happens {blah}%456 to contain happened punctuations universal rights of right contained."
+print(text_process(sample_text))
